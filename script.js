98bb4d782a654e8c9fe48f6435db6a1b58dcb965
bgn422yt7s-alt <bgn422yt7s@privaterelay.appleid.com>
2026-05-24 19:59:22 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const track = document.getElementById("track");
 const section = document.querySelector(".pin-section");
 
 let maxScroll = 0;
+let current = 0;
+let target = 0;
 
 function update() {
   maxScroll = track.scrollWidth - window.innerWidth;
@@ -11,16 +13,22 @@ window.addEventListener("resize", update);
 update();
 
 window.addEventListener("scroll", () => {
-
   const top = section.offsetTop;
   const height = section.offsetHeight;
   const scrollY = window.scrollY;
 
   let progress = (scrollY - top) / (height - window.innerHeight);
-
   progress = Math.max(0, Math.min(1, progress));
 
-  const x = -progress * maxScroll;
-
-  track.style.transform = `translate3d(${x}px,0,0)`;
+  target = -progress * maxScroll;
 });
+
+function animate() {
+  current += (target - current) * 0.06;
+
+  track.style.transform = `translate3d(${current}px,0,0)`;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
